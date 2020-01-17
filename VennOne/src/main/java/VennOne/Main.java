fe0d5fdae673f52ab085d0275fbf37a4f75f2cc7
marcoywm <marcoywm@hotmail.com>
2020-01-17 00:12:16 -0500

    MasterTest3

--- VennOne/src/main/java/VennOne/Main.java
+++ VennOne/src/main/java/VennOne/Main.java
@@ -6,14 +6,14 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("tst1");
 		System.out.println("tst1");
-<<<<<<< HEAD
+
 		System.out.println("MasterOne Test1");
 		System.out.println("MasterTwo Test2");
-		
-=======
+
 		System.out.println("This is branch Test1");
 		System.out.println("This is branch Test2");
->>>>>>> refs/heads/BranchVennOne
+		System.out.println("This is Master Test1");
+		System.out.println("This is Masater Test2");
 		
 	}
 
